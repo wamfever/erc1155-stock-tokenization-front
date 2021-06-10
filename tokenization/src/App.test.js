cf2254cb990856bc05689164dbc2bff5755e15b5
Varga Elod <vargaelod23@gmail.com>
2021-06-10 18:57:03 +0300

    Add minor test change

--- tokenization/src/App.test.js
+++ tokenization/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the Connect button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Connect Your MetaMask!/i);
   expect(linkElement).toBeInTheDocument();
 });
